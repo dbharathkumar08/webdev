92e5134a6915eb6e86074420ca3b17fd402b28eb
isha112 <isha112@ccs.neu.edu>
2015-02-12 22:39:28 -0700

    3rd commit

--- essential/experiments/week3/js/validation.js
+++ essential/experiments/week3/js/validation.js
@@ -4,6 +4,34 @@
         $('#result').html(checkStrength($('#pwd1').val()))
     })
 
+    $("#sbmt").bind("click", function () {
+
+        var pwd1 = $("#pwd1").val();
+        var pwd2 = $("#pwd2").val();
+        if (pwd1 != pwd2) {
+            alert("Passwords do not match");
+        }
+        else if (pwd1 == '' || pwd2 == '') {
+            alert("password field must have some value");
+        }
+        else if (pwd1 == pwd2) {
+            alert("Both Passwords match");
+        }
+        
+    })
+
+
+    $('.link').on('mouseenter', function () {
+        if ($('.tooltip').is(':visible')) {
+            $('.tooltip').hide();
+        }
+        $(this).next().show();
+    })
+
+    $('.link').on('mouseout', function () {
+        $(this).next().hide();
+    })
+
     function checkStrength(password) {
         var strength = 0
         if (password.length < 6) {
@@ -39,13 +67,7 @@
 
     }
 
-    $("#sbmt").bind("click", function () {
-
-        var pwd1 = $("#pwd1").val();
-        var pwd2 = $("#pwd2").val();
-        if (pwd1 != pwd2) {
-            alert("Passwords do not match");
-        }
-    })
+    
+    
 
 });
