06265400374079eb5b63cc7c0e71da34aeede005
isha112 <isha112@ccs.neu.edu>
2015-02-22 05:28:03 -0700

    week5:5

---
+++ essential/experiments/week5/js/calculator.js
@@ -0,0 +1,18 @@
+﻿var app = angular.module('app', []);
+
+
+app.controller('CalculatorController', function ($scope) {
+
+    $scope.clear = function () {
+        $scope.number = '';
+        $scope.answer = '';
+    }
+    $scope.doSquare = function () {
+        
+        $scope.answer = $scope.number * $scope.number;
+    }
+
+    $scope.doCube = function () {
+        $scope.answer = $scope.number * $scope.number * $scope.number;
+    }
+});
